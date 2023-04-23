23d23626aeb4109d0ed605c9054b89f30eb53ba7
devshon <wjdgns200297@gmail.com>
2023-04-23 16:55:43 +0900

    feat: test2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-// test
+// test2
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }

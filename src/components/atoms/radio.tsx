af2328435ea9a7c8da23c9083864d7d6a13a3d9f
장재훈/FE <wkdwo16@gmail.com>
2023-05-03 10:16:03 +0900

    feat: input, radio update

---
+++ src/components/atoms/radio.tsx
@@ -0,0 +1,21 @@
+import style from './radio.module.css'
+
+interface Props {
+    name: string
+    value: string
+    title: string
+}
+
+const Radio: React.FC<Props> = ({ name, title, value }) => {
+
+    let id = `radio-${value}`
+
+    return (
+        <div className="flex items-center mb-1">
+            <input type="radio" id={id} name={name} value={value} className="w-4 h-4 text-blue-600 rounded focus:ring-blue-500" />
+            <label htmlFor={id} className="ml-2 text-sm font-medium">{title}</label>
+        </div>
+    )
+}
+
+export default Radio

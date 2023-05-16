b6e4c203f8a2a23f300d1fe053faf4c967f563fe
jangjaehoon <wkdwo16@naver.com>
2023-05-16 14:07:51 +0900

    feat: 미팅방식 페이지

--- src/components/organisms/modal.tsx
+++ src/components/organisms/modal.tsx
@@ -1,4 +1,5 @@
-import { MouseEventHandler } from "react"
+import SquareBtn from "@atoms/squareBtn"
+import { MouseEvent, MouseEventHandler } from "react"
 
 interface Props {
     show: boolean
@@ -14,15 +15,15 @@ interface Props {
 const Modal: React.FC<Props> = ({ show, type = 'alert', title, content, onConfirm, onCancel, confirmTxt = '확인', closeTxt = '나가기' }) => {
 
     const confirm = (
-        <div className="items-center px-4 py-3 grid grid-cols-2 gap-2">
-            <button className="px-4 py-2 bg-gray4 text-black font-medium rounded-md" onClick={onCancel}>{closeTxt}</button>
-            <button className="px-4 py-2 bg-primary1 text-white font-medium rounded-md" onClick={onConfirm}>{confirmTxt}</button>
+        <div className="grid grid-cols-2">
+            <SquareBtn type={"grey"} onClick={onCancel} children={"나가기"} />
+            <SquareBtn type={"point"} onClick={onConfirm} children={"확인"} />
         </div>
     )
 
     const alert = (
-        <div className="items-center px-4 py-3">
-            <button className="px-4 py-2 bg-primary1 text-white rounded-md w-full" onClick={onCancel}>{closeTxt}</button>
+        <div className="grid grid-cols-1">
+            <SquareBtn type={"point"} onClick={onCancel} children={"나가기"} />
         </div>
     )
 
@@ -32,16 +33,17 @@ const Modal: React.FC<Props> = ({ show, type = 'alert', title, content, onConfir
 
     return (
         <div className="fixed top-0 left-0 w-full h-full overflow-x-hidden overflow-y-auto">
-            <div className="absolute w-full h-full bg-gray-300 opacity-50"></div>
-            <div className="relative top-20 mx-auto p-5 border w-80 md:w-96 lg:w-96 shadow-lg rounded-md bg-white">
-                <button className="absolute top-5 right-5 w-6 h-6 bg-contain bg-center bg-no-repeat bg-[url('/images/x.png')]" onClick={onCancel}></button>
-                <div className="mt-3 text-center">
+            <div className="absolute w-full h-full bg-slate-700 opacity-50"></div>
+
+            <div className="relative top-24 mx-auto border-inherit w-[250px] rounded-2xl overflow-hidden bg-white">
+                <button className="absolute top-2.5 right-2.5 w-6 h-6 bg-contain bg-center bg-no-repeat bg-[url('/images/x.png')]" onClick={onCancel}></button>
+                <div className="px-5 py-[50px] text-center">
                     <h3 className="text-lg text-gray-900">{title}</h3>
-                    <div className="mt-3 px-7 py-3">
-                        <p className="text-sm text-gray-500">{content}</p>
+                    <div>
+                        <p className="whitespace-pre-line text-sm font-medium text-black-500">{content}</p>
                     </div>
-                    {type == 'confirm' ? confirm : alert}
                 </div>
+                {type == 'confirm' ? confirm : alert}
             </div>
         </div>
     )

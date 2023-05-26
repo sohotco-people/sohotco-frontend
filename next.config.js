42a8084a90b9e4ee5b79bb3baa93fff47fdd9450
yeonjeongJ <eliz.jang.28@gmail.com>
2023-05-26 18:37:29 +0900

    Add: 프로젝트 생성 페이지 내 에디터 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const removeImports = require('next-remove-imports')
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
@@ -6,4 +8,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = removeImports({ ...nextConfig })
